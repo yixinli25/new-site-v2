060a49647a2d7921b47d7a2386bbb682e1659885
Yixin Li <yixin0325@gmail.com>
2024-07-30 16:02:23 -0400

    feat: set up project

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import './style.css';
+
+document.body.addEventListener("mousemove", evt => {
+    const mouseX = evt.clientX;
+    const mouseY = evt.clientY;
+  
+    gsap.set(".cursor", {
+      x: mouseX,
+      y: mouseY });
+  
+  
+    gsap.to(".shape", {
+      x: mouseX,
+      y: mouseY,
+      stagger: -0.1 });
+  
+  });  
