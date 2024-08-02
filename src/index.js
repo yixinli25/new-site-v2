9eb4467d07cbb21af3646fd906ee36d8510c96ff
Yixin Li <yixin0325@gmail.com>
2024-08-02 13:13:59 -0400

    feat: add loading layer

--- src/index.js
+++ src/index.js
@@ -14,4 +14,20 @@ document.body.addEventListener("mousemove", evt => {
       y: mouseY,
       stagger: -0.1 });
   
-  });  
+  });
+
+window.addEventListener('load', function() {
+  setTimeout(function() {
+    document.querySelector('.content .loading').classList.add('hide');
+  }, 1200);
+
+  setTimeout(function() {
+    document.querySelector('.content .loading').classList.add('d-none');
+    document.querySelector('.content figure').classList.remove('d-none');
+  }, 1500);
+
+  setTimeout(function() {
+    document.querySelector('.content figure').classList.remove('hide');
+    document.querySelector('.content figure').classList.add('show');
+  }, 2000);
+});
